8edf35e1046359b100394ae567d26d673a0b3402
Parker Catalano <catalano.work@gmail.com>
2017-10-17 16:52:08 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-
+  array = ["Beevus", "Butthead", "Cat", "Dog"]
+  array.push("Hey_Arnorld")
 end
 
 def add_element_to_start_of_array(array, element)
